3bff5d62258efae0e511341c80f0597240ffd485
haerong22 <haerong22@naver.com>
2025-09-16 23:58:05 +0900

    #7 n8n: data processing with python

---
+++ n8n/agent-usecases/data_processing.py
@@ -0,0 +1,27 @@
+import zerox
+import os
+import asyncio
+from dotenv import load_dotenv
+import nest_asyncio
+nest_asyncio.apply()
+
+load_dotenv()
+
+model = "gpt-4o-2024-11-20"
+
+async def main():
+    for file_path in os.listdir("./documents"):
+        file_path = f"./documents/{file_path}"
+
+    kwargs = {}
+    custom_system_prompt = None
+    select_pages = None
+    output_dir = "./output"
+    result = await zerox(file_path = file_path, model = model, output_dir = output_dir,
+                         custom_system_prompt = custom_system_prompt, select_pages = select_pages, **kwargs)
+    
+    print(f'{file_path} is done')
+    return result
+
+
+result = asyncio.run(main())
