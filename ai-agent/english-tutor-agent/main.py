33fdc31b89a940606cf57aaf2d897cafa495f5e4
haerong22 <haerong22@naver.com>
2025-12-19 17:54:55 +0900

    #8 english tutor agent: english tutor bot

---
+++ ai-agent/english-tutor-agent/main.py
@@ -0,0 +1,33 @@
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+from env import TELEGRAM_BOT_TOKEN
+from tutor_crew import EnglishTutorCrew, add_to_conversation
+
+
+async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    if update.message is None or update.message.text is None:
+        return
+
+    user_message = update.message.text
+
+    chatbot_crew = EnglishTutorCrew()
+
+    result = chatbot_crew.crew().kickoff(inputs={"message": user_message})
+
+    bot_response = result.raw
+    add_to_conversation(user_message, bot_response)
+
+    await update.message.reply_text(bot_response)
+
+
+app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT, handler))
+
+app.run_polling()
