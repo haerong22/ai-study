cda7378d1145f57fb77311f0d474f92704bd223b
haerong22 <haerong22@naver.com>
2026-01-22 00:37:42 +0900

    #12 koding agent: call llm

--- koding-agents/src/main/kotlin/Main.kt
+++ koding-agents/src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
 package org.example
 
-fun main() {
-    val apiKey = System.getenv("OPENAI_API_KEY")
+import ai.koog.prompt.dsl.prompt
+import ai.koog.prompt.executor.clients.openai.OpenAIModels
+import ai.koog.prompt.executor.llms.all.simpleOpenAIExecutor
 
-    println(apiKey.take(15))
+suspend fun main() {
+    val apiKey = System.getenv("OPENAI_API_KEY") ?: error("Missing OPENAI_API_KEY")
+
+    val executor = simpleOpenAIExecutor(apiKey)
+
+    print("User: ")
+    val userPrompt = readln()
+
+    val prompt = prompt(id = "hello-koog") {
+        system("당신은 코딩 에이전트 입니다.")
+        user(userPrompt)
+    }
+
+    val response = executor.execute(
+        prompt = prompt,
+        model = OpenAIModels.Chat.GPT5Mini
+    )
+
+    println("Assistant: ${response.last().content}")
 }
