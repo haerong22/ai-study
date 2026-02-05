11178958fce956ae1898ff760c76385700180979
haerong22 <haerong22@naver.com>
2026-02-06 00:30:50 +0900

    #13 gourmet bot: init

---
+++ gourmetbot/src/test/kotlin/org/example/gourmetbot/GourmetbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.gourmetbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GourmetbotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
